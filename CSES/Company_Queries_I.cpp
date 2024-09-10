f27df0b3396f936cd6ca4e5924d0c0664a8521f5
maianhpham123 <167604421+aCoderChild@users.noreply.github.com>
2024-09-10 23:32:39 +0700

    added files to repository

--- CSES/Company_Queries_I.cpp
+++ CSES/Company_Queries_I.cpp
@@ -1,7 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	
+void input(int &n, int &q, vector<int> &parent, vector<vector<int> > &queries) {
+    cin >> n >> q;
+    parent.resize(n-1);
+    queries.resize(q, vector<int>(2));
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> parent[i];
+    }
+
+    for (int i = 0; i < q; i++) {
+        cin >> queries[i][0];
+        cin >> queries[i][1];
+    }
+}
+
+int findParent(int a, int k, map<int, int> par) {
+    int parent = a;
+    for (int i = 0; i < k; i++) {
+        if(par.find(parent) != par.end()) parent = par[parent];
+        else {
+            return -1;
+        }
+    }
+    return parent;
+}
+
+void solution(int n, vector<int> parent, vector<vector<int> > queries) {
+    map<int, int> par;
+    for (int i = 2; i <= n; i++) {
+        par[i] = parent[i-2]; 
+    }
+
+    for (auto query : queries) {
+        cout << findParent(query[0], query[1], par) << endl;
+    }
+}
+
+int main() {
+    int n, q;
+    vector<int> parent;
+    vector<vector<int> > queries;
+
+    input(n, q, parent, queries);
+    solution(n, parent, queries);
+
+    return 0;
 }
